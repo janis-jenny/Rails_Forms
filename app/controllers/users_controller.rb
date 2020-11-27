e88e50da8320eef207b597c071c7e49f97a0fb13
thneves <tsneves11@gmail.com>
2020-11-27 12:46:20 -0300

    create first HTML form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
-    def new
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
-    def create
-    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end

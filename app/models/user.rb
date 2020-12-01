13e37272c1df1fb9a3662bc014dfc27dcacd32f1
Jenny C <63382832+janis-jenny@users.noreply.github.com>
2020-12-01 13:28:06 -0500

    Add uniqueness to email attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
-  validates :email, presence: true, length: { in: 6..20 }
-  validates :password, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, length: { in: 6..20 }
+  validates :password, presence: true
 end
